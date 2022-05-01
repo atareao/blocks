0c11acbeaf9c20f9f91e29595d51bb0c4cb4f410
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-05-01 11:11:31 +0200

    Add rel 0

--- ytblock/block.js
+++ ytblock/block.js
@@ -124,8 +124,10 @@
         },
         // <iframe width="560" height="315" src="https://www.youtube.com/embed/qn-6yLJaq8o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         save: function(props) {
+            //let blockProps = wp.blockEditor.useBlockProps.save();
+            let url = `https://www.youtube.com/embed/${props.attributes.ytId}?rel=0`;
+            props.attributes.src = url;
             let blockProps = wp.blockEditor.useBlockProps.save();
-            props.attributes.src = "https://www.youtube.com/embed/" + props.attributes.ytId;
             return wp.element.createElement(
                 "div",
                 {class: "containter"},
